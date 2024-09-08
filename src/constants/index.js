932f78ca3d10506f9d667b3a5c1e63819f534feb
Naman Rana <namanrana1828@gmail.com>
2024-09-09 00:26:03 +0530

    Remove unused navigation items in constants/index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -56,18 +56,6 @@ export const navigation = [{
         title: "Case Studies",
         url: "#roadmap",
     },
-    {
-        id: "4",
-        title: "New account",
-        url: "#signup",
-        onlyMobile: true,
-    },
-    {
-        id: "5",
-        title: "Sign in",
-        url: "#login",
-        onlyMobile: true,
-    },
 ];
 
 export const heroIcons = [homeSmile, file02, searchMd, plusSquare];
