89a406a267e0961e7e458372cdbbc2b6edf4ca1e
Naman Rana <namanrana1828@gmail.com>
2024-09-21 00:15:03 +0530

    Refactor routing configuration and add Roadmap component

--- src/constants/index.js
+++ src/constants/index.js
@@ -320,34 +320,35 @@ export const benefits = [{
     },
 ];
 
-export const socials = [{
-        id: "0",
-        title: "Discord",
-        iconUrl: discordBlack,
-        url: "#",
-    },
-    {
-        id: "1",
-        title: "Twitter",
-        iconUrl: twitter,
-        url: "#",
-    },
-    {
-        id: "2",
-        title: "Instagram",
-        iconUrl: instagram,
-        url: "#",
-    },
-    {
-        id: "3",
-        title: "Telegram",
-        iconUrl: telegram,
-        url: "#",
-    },
-    {
-        id: "4",
-        title: "Facebook",
-        iconUrl: facebook,
-        url: "#",
-    },
+export const socials = [
+    //{
+    //     id: "0",
+    //     title: "Discord",
+    //     iconUrl: discordBlack,
+    //     url: "#",
+    // },
+    // {
+    //     id: "1",
+    //     title: "Twitter",
+    //     iconUrl: twitter,
+    //     url: "#",
+    // },
+    // {
+    //     id: "2",
+    //     title: "Instagram",
+    //     iconUrl: instagram,
+    //     url: "#",
+    // },
+    // {
+    //     id: "3",
+    //     title: "Telegram",
+    //     iconUrl: telegram,
+    //     url: "#",
+    // },
+    // {
+    //     id: "4",
+    //     title: "Facebook",
+    //     iconUrl: facebook,
+    //     url: "#",
+    // },
 ];
