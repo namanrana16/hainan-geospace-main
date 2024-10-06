72f00aac4f97c883975d1ef5304482edadebfeaa
Naman Rana <namanrana1828@gmail.com>
2024-10-07 03:25:15 +0530

    Refactor text alignment in Benefits, Blog, and Collaboration components

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -47,7 +47,7 @@ const Roadmap = () => (
 
                   </div>
                   <h4 className="h4 mb-4">{item.title}</h4>
-                  <p className="body-2 text-n-4">{item.text}</p>
+                  <p className="body-2 text-n-4 text-justify">{item.text}</p>
                 </div>
               </div>
             </div>
